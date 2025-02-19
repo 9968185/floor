539d977f097db5f374f87fa88be26fae34ce6903
9968185 <9968185@gmail.com>
2025-02-19 21:09:54 +0800

    Modify whose.cpp

--- whose.cpp
+++ whose.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Identify almost fly sense including.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
